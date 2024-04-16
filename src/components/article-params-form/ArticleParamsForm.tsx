e80a5cc6f81990bd83b601e6067aa318e7accaca
Stepan Sukhachev <droiderlinux@yahoo.com>
2024-04-16 21:55:22 +0300

    feat: add text color costomization

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,23 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useRef, useState } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 import clsx from 'clsx';
 import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+import { ArticleStateType, fontColors } from 'src/constants/articleProps';
+import { Select } from '../select';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (param: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [fontColor, setFontColor] = useState(articleState.fontColor);
 	const rootRef = useRef(null);
 
 	useOutsideClickClose({
@@ -17,13 +28,24 @@ export const ArticleParamsForm = () => {
 		onChange: setIsOpen,
 	});
 
+	const formSubmitHandler = (e: FormEvent) => {
+		e.preventDefault();
+		setArticleState({ ...articleState, fontColor: fontColor });
+	};
+
 	return (
 		<>
 			<ArrowButton onClick={setIsOpen} isOpenForm={isOpen} />
 			<aside
 				ref={rootRef}
 				className={clsx(styles.container, isOpen && styles.container_open)}>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={formSubmitHandler}>
+					<Select
+						options={fontColors}
+						selected={fontColor}
+						title='цвет'
+						onChange={setFontColor}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
